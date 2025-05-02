e7e4a578f79192e15b02e430be0e99b23bae00e3
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-02 09:52:55 +0200

    Create registration.sh

---
+++ registration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
+source $PATH/env
+
+block=$(curl -s -X POST -H 'Content-Type: application/json' \
+-d '{"jsonrpc":"2.0","method":"node_getL2Tips","params":[],"id":67}' \
+http://localhost:8080 | jq -r ".result.proven.number")
+
+proof=$(curl -s -X POST -H 'Content-Type: application/json' \
+-d '{"jsonrpc":"2.0","method":"node_getArchiveSiblingPath","params":["$block","$block"],"id":67}' \
+http://localhost:8080 | jq -r ".result")
+
+echo address: $WALLET
+echo block: $block
+echo proof: $proof
