f53d27a99cf61ff81446d4d88bc2b3a90f8d748b
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-02 09:50:07 +0200

    Create start-cli.sh

---
+++ start-cli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
+source /root/.bash_profile
+source #path/env
+
+aztec start --node --archiver --sequencer \
+  --network alpha-testnet \
+  --l1-rpc-urls $RPC  \
+  --l1-consensus-host-urls $BEACON \
+  --sequencer.validatorPrivateKey $PK \
+  --sequencer.coinbase $WALLET \
+  --p2p.p2pIp $IP
