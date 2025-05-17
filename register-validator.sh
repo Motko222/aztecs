b9ff2eadb3e7c060aca976b330e815f378f78c7e
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-17 20:57:26 +0200

    Update register-validator.sh

--- register-validator.sh
+++ register-validator.sh
@@ -5,10 +5,12 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 source /root/.bash_profile
 
+sleep $DELAY
+
 aztec add-l1-validator \
   --l1-rpc-urls $RPC \
   --private-key $PK \
   --attester $WALLET \
   --proposer-eoa $WALLET \
   --staking-asset-handler 0xF739D03e98e23A7B65940848aBA8921fF3bAc4b2 \
-  --l1-chain-id 11155111
+  --l1-chain-id 11155111 | head -10
