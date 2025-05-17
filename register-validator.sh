033f684f8372db30d8f87d12967b567583df034a
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-17 20:59:54 +0200

    Update register-validator.sh

--- register-validator.sh
+++ register-validator.sh
@@ -5,8 +5,28 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 source /root/.bash_profile
 
+echo $(date --utc +%FT%TZ) started, waiting $DELAY
 sleep $DELAY
 
+echo $(date --utc +%FT%TZ) attempt 1
+aztec add-l1-validator \
+  --l1-rpc-urls $RPC \
+  --private-key $PK \
+  --attester $WALLET \
+  --proposer-eoa $WALLET \
+  --staking-asset-handler 0xF739D03e98e23A7B65940848aBA8921fF3bAc4b2 \
+  --l1-chain-id 11155111 | head -10
+
+  echo $(date --utc +%FT%TZ) attempt 2
+aztec add-l1-validator \
+  --l1-rpc-urls $RPC \
+  --private-key $PK \
+  --attester $WALLET \
+  --proposer-eoa $WALLET \
+  --staking-asset-handler 0xF739D03e98e23A7B65940848aBA8921fF3bAc4b2 \
+  --l1-chain-id 11155111 | head -10
+
+  echo $(date --utc +%FT%TZ) attempt 3
 aztec add-l1-validator \
   --l1-rpc-urls $RPC \
   --private-key $PK \
