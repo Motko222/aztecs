d14c0148b98ae2ae976cdb28de5558ce8a8d641b
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-19 08:36:51 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -10,7 +10,7 @@ version=$(aztec -V | sed 's/\r//g')
 service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
 errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "rror|ERR")
 latest=$(curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"node_getL2Tips","params":[],"id":67}' http://localhost:8080 | jq -r ".result.latest.number")
-peerid=$(docker logs $(docker ps -q --filter ancestor=aztecprotocol/aztec:alpha-testnet | head -n 1) 2>&1 | grep -i "peerId" | grep -o '"peerId":"[^"]*"' | cut -d'"' -f4 | head -n 1)
+peerid=$(docker logs $(docker ps -q --filter ancestor=aztecprotocol/aztec:latest | head -n 1) 2>&1 | grep -i "peerId" | grep -o '"peerId":"[^"]*"' | cut -d'"' -f4 | head -n 1)
 
 status="ok" && message="latest=$latest"
 [ $errors -gt 500 ] && status="warning" && message="latest=$latest errors=$errors";
