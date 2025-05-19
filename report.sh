1492f83150ca9873a504ec0be9a3c862aee2c21a
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-19 08:32:41 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -10,6 +10,7 @@ version=$(aztec -V | sed 's/\r//g')
 service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
 errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "rror|ERR")
 latest=$(curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"node_getL2Tips","params":[],"id":67}' http://localhost:8080 | jq -r ".result.latest.number")
+peerid=$(docker logs $(docker ps -q --filter ancestor=aztecprotocol/aztec:alpha-testnet | head -n 1) 2>&1 | grep -i "peerId" | grep -o '"peerId":"[^"]*"' | cut -d'"' -f4 | head -n 1)
 
 status="ok" && message="latest=$latest"
 [ $errors -gt 500 ] && status="warning" && message="latest=$latest errors=$errors";
@@ -33,9 +34,10 @@ cat >$json << EOF
         "message":"$message",
         "service":$service,
         "errors":$errors,
-        "url":"$RPC $BEACON",
+        "url":"$RPC $BEACON $peerid",
         "balance":"$balance",
-        "latest":"$latest"
+        "latest":"$latest",
+        "peerid":"$peerid"
   }
 }
 EOF
