39a37feefd6e60a20a0ad8776634291e8a74d712
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-28 08:17:43 +0200

    Update register-validator.sh

--- register-validator.sh
+++ register-validator.sh
@@ -4,15 +4,20 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 source /root/.bash_profile
+log=/root/logs/aztec-validator-add
+
 
 echo $(date --utc +%FT%TZ) started, waiting $DELAY
 sleep $DELAY
 
 echo $(date --utc +%FT%TZ) attempt 1
-aztec add-l1-validator \
+response=$(aztec add-l1-validator \
   --l1-rpc-urls $RPC \
   --private-key $PK \
   --attester $WALLET \
   --proposer-eoa $WALLET \
   --staking-asset-handler 0xF739D03e98e23A7B65940848aBA8921fF3bAc4b2 \
-  --l1-chain-id 11155111 | head -10
+  --l1-chain-id 11155111 | head -4)
+  
+
+  
