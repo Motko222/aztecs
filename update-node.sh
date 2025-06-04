881169c4f6acfc3581078b1f30971646004756b1
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-04 08:25:34 +0200

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -2,5 +2,7 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 
+read -p "Version? " version
+
 cd /root/.aztec/bin
-./aztec-up latest
+./aztec-up -v $version
