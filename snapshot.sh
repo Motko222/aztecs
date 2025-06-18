76eddafd6b69bac34eb3607d9dbce392464415db
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-18 13:22:02 +0200

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -2,6 +2,9 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 
+read -p "Sure? " c
+case $c in y|Y) ;; *) exit ;; esac
+
 cd $path
 ./stop.sh
 
