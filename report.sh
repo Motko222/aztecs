f113eaa7252ce16834eb62aff6a8a98a75a627bf
Motko <77466238+Motko222@users.noreply.github.com>
2025-11-10 23:21:09 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -13,7 +13,7 @@ latest=$(curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0
 peerid=$(docker logs $(docker ps -q --filter ancestor=aztecprotocol/aztec:latest | head -n 1) 2>&1 | grep -i "peerId" | grep -o '"peerId":"[^"]*"' | cut -d'"' -f4 | head -n 1)
 
 
-state=$(journalctl -u $folder.service --no-hostname -o cat | grep "World state updated with L2 block " | tail -1 | awf -F "World state updated with L2 block " '{print $2}' | awk '{print $$1}')
+state=$(journalctl -u $folder.service --no-hostname -o cat | grep "World state updated with L2 block " | tail -1 | awk -F "World state updated with L2 block " '{print $2}' | awk '{print $$1}')
 
 status="ok" && message=""
 [ $errors -gt 500 ] && status="warning" && message="too many errors ($errors/h)";
