bc59cb4cf2f9aa496b385a8f6b26b5e7a507ce5b
Motko <77466238+Motko222@users.noreply.github.com>
2025-11-10 21:22:11 +0100

    Create approve.sh

---
+++ approve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+source $path/env
+cd $path
+
+cast send 0x139d2a7a0881e16332d7D1F8DB383A4507E1Ea7A "approve(address,uint256)" 0xebd99ff0ff6677205509ae73f93d0ca52ac85d67 200000ether \
+ --private-key "$PK" --rpc-url $RPC
